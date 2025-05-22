e3d84027e92be3cec8b005eb1cfcc0292f8de84a
Baratão <joao.bertoncini.silva@escola.pr.gov.br>
2025-05-22 11:49:40 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,29 +1,35 @@
 criaCartao(
-    'QUANDO O POU FOI LANÇADO?',
-    '2013.'
+    '1° PERGUNTA',
+        'QUANDO O POU FOI LANÇADO?',
+        '2013',
 )
 
 criaCartao(
-    'O que seria o zero da função?',
-    'O valor onde o gráfico cruza o eixo da abscissa, ou ainda o valor de x para quando y for igual a 0.'
+    '2° PERGUNTA',
+    'O POU É UM?',
+    'ALIENÍGENA',
 )
 
 criaCartao(
-    'Qual a definição de média aritmética?',
-    'Resultado da divisão do somatório dos números dados pela quantidade de números somados.'
+    '3° PERGUNTA',
+    'QUAL É O LEVEL MÁXIMO NO JOGO DO POU?',
+    '160',
 )
 
 criaCartao(
-    'Como se calcula a área de um triângulo qualquer?',
-    'Multiplique a base pela altura e divida por dois.'
+    '4° PERGUNTA',
+    'QUAL A COR ORIGINAL DO POU?',
+    'MARROM',
 )
 
 criaCartao(
-    'O que define o conjunto dos números racionais, dê exemplos?',
-    'Números que podem ser escritos na forma de fração. Por exemplo, 3/4, 10/2, -8/2'
+    '5° PERGUNTA',
+    'QUE BARULHO O POU FAZ?',
+    'NHÁ',
 )
 
 criaCartao(
-    'Quando utilizo o teorema de Pitágoras?',
-    'Para encontrar um dos lados de um triângulo retângulo, sendo dado os outros dois lados. h² = c² + c².'
+    '6° PERGUNTA',
+    'QUAL O MELHOR MINIGAME DO POU?',
+    'CLIFF JUMP',
 )
